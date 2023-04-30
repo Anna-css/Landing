b15754d72d0d5a8585722ac4f315411f22e10a79
Anna-css <124602700+Anna-css@users.noreply.github.com>
2023-04-30 12:06:09 +0300

    menu

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const btnToggleMenu = document.querySelector ('.header__toggle');
 const headerMenu = document.querySelector('.menu');
 
 const toggleMenu = function(){
-  headerMenu.classList.toggle('menu__shown');
+  headerMenu.classList.toggle('menu_shown');
 
 }
 btnToggleMenu.addEventListener('click',toggleMenu);
